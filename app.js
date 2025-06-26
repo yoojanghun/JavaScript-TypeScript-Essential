290151b609589ab1543db144c0da1ad61b364c29
yoojanghun <yoojanghun@naver.com>
2025-06-26 17:19:10 +0900

    두 개의 화면을 가진 웹앱

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const ajax = new XMLHttpRequest();
 // "https://api.hnpwa.com/v0/news/1.json": 호출할 API 주소
 // false: 동기(synchronous) 요청:  이 요청이 완료될 때까지 다음 코드가 절대 실행되지 않음
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+const content = document.createElement("div");
+const container = document.getElementById("root");
 ajax.open("GET", NEWS_URL, false);
 
 // 실제로 서버에 요청을 보냄
@@ -19,17 +22,36 @@ ajax.send();
 // 응답 결과 (JSON 형식의 문자열)를 콘솔에 출력. 
 // JSON(Javascript Object Notation): 자바스크립트 객체 형식을 본뜬 "데이터 교환용 텍스트 형식"
 // 클라이언트-서버 간 데이터 주고 받을 때, 외부 API 호출할 때 주로 씀
-console.log(ajax.response);
+// console.log(ajax.response);
 
 // JSON을 parse로 객체로 바꿈(JSON은 객체로 변환 가능)
 const newsFeed = JSON.parse(ajax.response);
-
 const ul = document.createElement("ul");
+
+window.addEventListener("hashchange", function() {
+    const id = location.hash.substring(1);
+
+    ajax.open("GET", CONTENT_URL.replace("@id", id), false);
+    ajax.send();
+
+    const newsContent = JSON.parse(ajax.response);
+    const title = document.createElement("h1");
+
+    title.textContent = newsContent.title;
+
+    content.appendChild(title);
+})
 
 for(let i = 0; i < 10; i++){
     const li = document.createElement("li");
-    li.textContent = newsFeed[i].title;
+    const a = document.createElement("a");
+
+    a.href = `#${newsFeed[i].id}`;
+    a.textContent = `${newsFeed[i].title} // comments num : ${newsFeed[i].comments_count}`;
+
     ul.appendChild(li);
+    li.appendChild(a);
 }
 
-document.getElementById("root").appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
