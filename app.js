65953abcd7e69466225167a911bda6b54ddfda4e
yoojanghun <yoojanghun@naver.com>
2025-06-29 21:25:23 +0900

    페이징 구현하기

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 const content = document.createElement("div");
 const container = document.getElementById("root");
+const store = {
+    currentPage: 1,
+};
 
 function getData(url) {
     ajax.open("GET", url, false);
@@ -42,7 +45,11 @@ function router() {
     if(routePath === "") {
         newsFeed();
     }
-    else{
+    else if(routePath.indexOf("#/page/") >= 0){
+        store.currentPage = Number(routePath.substring(7));
+        newsFeed();
+    }
+    else {
         newsDetail();
     }
 }
@@ -53,10 +60,10 @@ function newsFeed() {
     
     newsList.push("<ul>");
     
-    for(let i = 0; i < 10; i++){
+    for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++){
         newsList.push(`
             <li>
-                <a href="#${newsFeed[i].id}">
+                <a href="#/show/${newsFeed[i].id}">
                     ${newsFeed[i].title} (${newsFeed[i].comments_count})
                 </a>
             </li>
@@ -64,12 +71,18 @@ function newsFeed() {
     }
     
     newsList.push("</ul>");
+    newsList.push(`
+        <div>
+            <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+            <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
+        </div>
+    `);
     
     container.innerHTML = newsList.join("");
 }
 
 function newsDetail() {
-    const id = location.hash.substring(1);
+    const id = location.hash.substring(7);
 
     const newsContent = getData(CONTENT_URL.replace("@id", id));
 
@@ -77,7 +90,7 @@ function newsDetail() {
         <h1>${newsContent.title}</h1>
 
         <div>
-            <a href="#">목록으로</a>
+            <a href="#/page/${store.currentPage}">목록으로</a>
         </div>
     `;
 }
