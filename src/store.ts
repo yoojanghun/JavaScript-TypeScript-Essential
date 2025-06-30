7767c79369346b3a2b82ed03ff27e8432d9b47fc
yoojanghun <yoojanghun@naver.com>
2025-06-30 18:55:58 +0900

    XHR to Fetch & Promise

--- src/store.ts
+++ src/store.ts
@@ -25,11 +25,11 @@ export class Store implements NewsStore {
     return this._currentPage > 1 ? this._currentPage - 1 : 1;
   }
   
-  get numberOfFeed(): number {
+  get numberOfFeed() {
     return this.feeds.length;
   }
 
-  get hasFeeds(): boolean {
+  get hasFeeds() {
     return this.feeds.length > 0;
   }
 
@@ -41,14 +41,14 @@ export class Store implements NewsStore {
     return this.feeds;
   }
 
-  setFeeds(feeds: NewsFeed[]): void {
+  setFeeds = (feeds: NewsFeed[]) => {
     this.feeds = feeds.map(feed => ({
       ...feed, 
       read: false
     }));
   }
 
-  makeRead(id: number): void {
+  makeRead(id: number) {
     const feed = this.feeds.find((feed: NewsFeed) => feed.id === id);
 
     if (feed) {
