c2d4e5b53189f54b42278d82280775805f946ed7
yoojanghun <yoojanghun@naver.com>
2025-06-30 16:06:09 +0900

    변수에 타입 작성하기

--- app.ts
+++ app.ts
@@ -1,21 +1,30 @@
 // HTTP 요청: 웹 브라우저나 프로그램이 서버에 "무언가 해달라"고 보내는 요청
 // GET(서버에서 데이터 가져오기), POST(서버에 새로운 데이터 보내기), DELETE(서버에 데이터 삭제하기) 등
-
-
 // API(Application Programming Interface): 서버가 외부에 제공하는 기능/데이터 출입구
 
+// type Alias 방법을 통해 객체의 타입을 설정한다.
+type Store = {
+    currentPage: number;
+    feeds: NewsFeed[];      // NewsFeed 유형의 데이터가 들어가는 배열
+}
+type NewsFeed = {
+    id: number;
+    comments_count: number;
+    url: string;
+    user: string;
+    time_ago: string;
+    points: number;
+    title: string;
+    read?: boolean;         // 있을 때도 있고 없을 때도 있는 속성
+}
 
-// 브라우저에서 서버에 HTTP 요청을 보내고 응답을 받을 때 사용하는 오래된 방식의 API
-const ajax = new XMLHttpRequest();
+const ajax: XMLHttpRequest = new XMLHttpRequest();
 
-// "GET": 데이터를 가져오기 위한 GET 방식
-// "https://api.hnpwa.com/v0/news/1.json": 호출할 API 주소
-// false: 동기(synchronous) 요청:  이 요청이 완료될 때까지 다음 코드가 절대 실행되지 않음
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 const content = document.createElement("div");
-const container = document.getElementById("root");
-const store = {
+const container: (HTMLElement | null) = document.getElementById("root");
+const store: Store = {
     currentPage: 1,
     feeds: []
 };
@@ -23,12 +32,12 @@ const store = {
 function getData(url) {
     ajax.open("GET", url, false);
     
-    // 실제로 서버에 요청을 보냄
     ajax.send();
 
     return JSON.parse(ajax.response);
 }
 
+// 타입 추론: for 문에서 i 에 타입 지정을 안 해줘도 타입스크립트가 i에 들어가는 type이 number일 것이라고 추정함
 function makeFeeds(feeds) {
     for(let i = 0; i < feeds.length; i++) {
         feeds[i].read = false;
@@ -36,12 +45,20 @@ function makeFeeds(feeds) {
     return feeds;
 }
 
-// 응답 결과 (JSON 형식의 문자열)를 콘솔에 출력. 
+// container엔 HTML 요소 또는 Null이 들어가는 것이 가능함.
+// container에 Null이 들어가면 innerHTML 속성을 사용하는 것이 불가능.
+// container가 HTML요소일 때만 innerHTML을 사용하라고 명시해야 함.
+function updateView(html) {
+    if(container) {
+        container.innerHTML = html;
+    }
+    else{
+        console.error("최상위 컨테이너가 없어 UI를 진행하지 못합니다.");
+    }
+}
+
 // JSON(Javascript Object Notation): 자바스크립트 객체 형식을 본뜬 "데이터 교환용 텍스트 형식"
 // 클라이언트-서버 간 데이터 주고 받을 때, 외부 API 호출할 때 주로 씀
-// console.log(ajax.response);
-
-// JSON을 parse로 객체로 바꿈(JSON은 객체로 변환 가능)
 const ul = document.createElement("ul");
 
 window.addEventListener("hashchange", router);
@@ -63,7 +80,7 @@ function router() {
 }
 
 function newsFeed() {
-    let newsFeed = store.feeds;
+    let newsFeed: NewsFeed[] = store.feeds;
     const newsList = [];
     let template = `
         <div class="bg-gray-600 min-h-screen">
@@ -120,7 +137,7 @@ function newsFeed() {
     template = template.replace("{{__prev_page__}}", store.currentPage > 1 ? store.currentPage - 1 : 1);
     template = template.replace("{{__next_page__}}", store.currentPage + 1);
 
-    container.innerHTML = template;
+    updateView(template);
 }
 
 function newsDetail() {
@@ -182,5 +199,5 @@ function newsDetail() {
         return commentString.join("");
     }
 
-    container.innerHTML = template.replace('{{__comments__}}', makeComment(newsContent.comments));
+    updateView(template);
 }
